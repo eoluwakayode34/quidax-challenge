f106ed7fb33d93b6169aa75069f32277a85f3900
unknown <eoluwakayode34@gmail.com>
2020-02-25 15:31:22 +0100

    search responsive for mobile menu

--- js/app.js
+++ js/app.js
@@ -17,8 +17,30 @@ document.querySelector('.sidebar__toggle').addEventListener('click', function(){
 
 
 document.querySelector('.search-toggle').addEventListener('click', function(){
-    document.querySelector('')
+    document.querySelector('.user-nav-search ').style.display = 'none';
+    document.querySelector('.user-nav-book ').style.display = 'none';
+    document.querySelector('.user-nav__notification ').style.display = 'none';
+    document.querySelector('.user-nav__img-box').style.display = 'none';
 
+    document.querySelector('.logo-box').style.display = 'none';
+    // document.querySelector('.search').style.display= 'flex';
+    // document.querySelector('.search').style.opacity= '1';
+
+    document.querySelector('.search').classList.add('search-bar-open');
+
+});
+
+document.querySelector('.search__toggle').addEventListener('click', function(){
+    document.querySelector('.user-nav-search ').style.display = 'flex';
+    document.querySelector('.user-nav-book ').style.display = 'flex';
+    document.querySelector('.user-nav__notification ').style.display = 'flex';
+    document.querySelector('.user-nav__img-box').style.display = 'flex';
+
+    document.querySelector('.logo-box').style.display = 'flex';
+    // document.querySelector('.search').style.display= 'flex';
+    // document.querySelector('.search').style.opacity= '1';
+
+    document.querySelector('.search').classList.remove('search-bar-open');
 
 })
 
